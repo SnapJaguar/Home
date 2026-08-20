fdbae1fdd6837a4d9785ca00fa92b0e3c48e9d2f
Snape <nik.sabri@mail.ru>
2026-08-20 13:16:24 +0300

    task№2 add

---
+++ 2work.py
@@ -0,0 +1,71 @@
+class Student:
+    def __init__(self, surname, name, gender):
+        self.surname = surname
+        self.name = name
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecture(self, lecturer, course, grade):
+        if (
+            isinstance(lecturer, Lecturer)
+            and course in self.courses_in_progress
+            and course in lecturer.courses_attached
+        ):
+            lecturer.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if (
+            isinstance(student, Student)
+            and course in self.courses_attached
+            and course in student.courses_in_progress
+        ):
+            student.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка'
+
+
+# Проверка из задания №1
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)     # []
+print(reviewer.courses_attached)     # []
+
+# Проверка из задания №2
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))  # None
+print(student.rate_lecture(lecturer, 'Java', 8))    # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))     # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
+
+# Дополнительная проверка
+print(reviewer.rate_hw(student, 'Python', 10))  # None
+print(student.grades)  # {'Python': [10]}
